335a52db76b84742336b1e56212b387c8f243210
Brittney Rose <brittneyrose677@gmail.com>
2023-10-29 22:13:03 -0500

    Final scripts.js

--- scripts.js
+++ scripts.js
@@ -19,38 +19,3 @@ document.addEventListener('DOMContentLoaded', function(){
 
 });
 
-
-
-
-
-
-
-
-
-// /* Add your JavaScript to this file */
-// "use strict";
-// console.log("hello")
-// document.addEventListener("DOMContentLoaded", function(){
-//     const form = document.querySelector("form");
-//     const mes = document.querySelector(".message");
-//     console.log("hello2")
-
-//     //mes.textContent = "Thank you! Your email address ${enterredEmail} has been added to our mailing list!";
-
-//     form.addEventListener("submit", function(event){
-//         event.preventDefault();
-//         var enterredEmail = document.getElementById("email").value;
-//         console.log(enterredEmail)
-
-//         message.textContent = "Thank you! Your email address ${enterredEmail} has been added to our mailing list!";
-
-//         // if (enterredEmail != none){
-//         //     message.textContent = "Thank you! Your email address" + enterredEmail + "has been added to our mailing list!";
-//         // }
-//         // else{
-//         //     message.textContent = "Please enter a valid email address.";
-//         // }
-
-//         // enterredEmail.value = "";
-//     });
-// });
